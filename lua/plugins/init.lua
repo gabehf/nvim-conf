bb10c66f29cbf3c446186bf5a5f07dc1765d716a
Gabe Farrell <gabe@mnrva.dev>
2024-04-10 00:31:28 -0400

    add silliness

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,7 +47,27 @@ return {
       },
     },
   },
-  -- {
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      renderer = {
+        icons = {
+          glyphs = {
+            git = { -- this is so stupid lmao
+              unstaged = "😴",
+              staged = "😎",
+              unmerged = "😈",
+              untracked = "😭",
+              renamed = "😶",
+              deleted = "😵",
+            },
+          },
+        },
+      },
+    },
+  },
+
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
