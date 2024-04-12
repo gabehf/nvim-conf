13707ed6f9a2832d11df851dc63a69eafe5ca5e3
Gabe Farrell <gabe@mnrva.dev>
2024-04-12 15:39:18 -0400

    theme wip

---
+++ lua/themes/your-pop-star.lua
@@ -0,0 +1,55 @@
+return {
+  base_16 = {
+    base00 = "#13121F", -- default bg
+    base01 = "#312C5E", -- Lighter bg, line num, status bar, etc
+    base02 = "#2d294a", -- selection bg
+    base03 = "#222222", -- comments, line highlight
+    base04 = "#25233C", -- dark foreground (status bar)
+    base05 = "#ffffff", -- Default foreground, caret, delimeters, ops
+    base06 = "#000000", -- Light foreground
+    base07 = "#aaaaaa", -- light background
+    base08 = "#F066EC", -- vars, xml tag, markup links, diff delete
+    base09 = "#38F0E8", -- booleans, constants, ints
+    base0A = "#999999", -- classes, markup bold, search text bg
+    base0B = "#42F032", -- strings, inherited class, markup code, diff insert
+    base0C = "#C9ECF5", -- support, regex, escape chars, markup quotes
+    base0D = "#555555", -- funcs, methods, attr ids, headings, html inner text
+    base0E = "#ffe405", -- keywords, markup italic, diff changed
+    base0F = "#ffffff", -- lua comma, golang delimeter
+  },
+  base_30 = {
+    white = "#F8F8F2",
+    darker_black = "#19192c",
+    black = "#141423",
+    black2 = "#1c1c31",
+    one_bg = "#23233d",
+    one_bg2 = "#2b2b4c",
+    one_bg3 = "#373760",
+    grey = "#414171", -- line numbers
+    grey_fg = "#4b4b83", -- comments
+    grey_fg2 = "#555594",
+    light_grey = "#6060a4",
+    red = "#FF5ff5", -- changed git files,
+    baby_pink = "#FF6E6E",
+    pink = "#FF6BCB",
+    line = "#2D2D4E",
+    green = "#50FA7B",
+    vibrant_green = "#20E3B2",
+    nord_blue = "#05C3FF",
+    blue = "#2CCCFF",
+    yellow = "#F1FA8C", -- README nvim-tree
+    sun = "#F2FA95",
+    purple = "#BD93F9",
+    dark_purple = "#a166f6",
+    teal = "#92A2D4",
+    orange = "#FFB86C",
+    cyan = "#2CCCFF",
+    statusline_bg = "#19192c",
+    lightbg = "#2b2b4c",
+    pmenu_bg = "#9A86FD",
+    folder_bg = "#9A86FD",
+    violet = "#9A86FD",
+    darkgreen = "#1B312E",
+    brownred = "#5D2932",
+  },
+}
